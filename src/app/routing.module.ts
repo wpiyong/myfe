508cc6cbbb44a280483e1b0cbc8b97e8fd887298
wpiyong <lylylyly@hotmail.com>
2018-01-05 13:13:03 -0500

    update

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HomeComponent } from './home/home.component';
